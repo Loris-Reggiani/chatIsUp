fe93283086b27712e4ed5f874487f2479f9a976c
Loris-Reggiani <loris.reggiani@epitech.eu>
2024-05-20 00:14:01 +0200

    changing config.ts so it's according to new server

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,4 +1,4 @@
-let apiUrl = 'https://voron.djnn.sh/api';
+let apiUrl = 'http://20.199.14.58/';
 
 if (import.meta.env.VITE_REACT_APP_NODE_ENV === 'development')
     apiUrl = import.meta.env.VITE_REACT_APP_LOCAL_API_URL as string;
