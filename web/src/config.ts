aa8d94dcaee105cb1b1cf96b437f41d9cbf3036f
Loris-Reggiani <loris.reggiani@epitech.eu>
2024-05-20 14:00:02 +0200

    update config.ts

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,7 +1,7 @@
 let apiUrl = 'http://20.199.14.58/';
 
-if (import.meta.env.VITE_REACT_APP_NODE_ENV === 'development')
-    apiUrl = import.meta.env.VITE_REACT_APP_LOCAL_API_URL as string;
+// if (import.meta.env.VITE_REACT_APP_NODE_ENV === 'development')
+//     apiUrl = import.meta.env.VITE_REACT_APP_LOCAL_API_URL as string;
 
 const config = {
     apiUrl,
